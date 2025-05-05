d23e3ce02c291071c27dc74f0934c551221d9843
= <168827531+ZNemuZ@users.noreply.github.com>
2025-05-05 12:39:49 +0900

    テスト用の画像を追加

--- src/components/Organims/FoodDetail.tsx
+++ src/components/Organims/FoodDetail.tsx
@@ -1,12 +1,41 @@
 import { useState } from 'react';
+import foodImage from '../../assets/gohan.jpg';
+import { useParams } from 'react-router-dom';
 
+const dummeyFoods = [
+  {
+    id: '001',
+    name: 'たまご',
+    category: '卵類',
+    exp: '2025/04/25',
+    image: foodImage,
+  },
+  {
+    id: '002',
+    name: 'にんじん',
+    category: '野菜',
+    exp: '2025/04/20',
+    image: foodImage,
+  },
+  {
+    id: '003',
+    name: '牛乳',
+    category: '飲料',
+    exp: '2025/04/22',
+    image: foodImage,
+  },
+];
 const FoodDetail = () => {
+  const { id } = useParams();
+  const food = dummeyFoods.find((item) => item.id === id);
   const [isEditing, setIsEditing] = useState(false);
-  const [name, setName] = useState('卵');
+  const [name, setName] = useState(food?.name || '');
 
   return (
     <div className="flex flex-col items-center p-6">
-      <div className="h-40 w-40 object-cover rouded mb-6">gazou</div>
+      <div className="h-40 w-40 object-cover rouded mb-6">
+        <img src={food?.image}></img>
+      </div>
       <div className="w-full max-w-md space-y-4">
         <button
           onClick={() => setIsEditing(!isEditing)}
